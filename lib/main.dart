ab9a4432fc4bf05f3c29e9ee8da5223d56b779e3
juancho108 <jjf108@gmail.com>
2019-06-14 13:06:01 -0300

    disenos terminados

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:disenos/src/pages/basico_page.dart';
 import 'package:disenos/src/pages/scroll_page.dart';
+import 'package:disenos/src/pages/botones_page.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //ESTO ES PARA IOS PORQUE LOS BOTONES DE LA BARRA SON NEGROS
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light
+        .copyWith(statusBarColor: Colors.transparent));
+
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Diseños',
-        initialRoute: 'scroll',
+        initialRoute: 'botones',
         routes: {
           'basico': (BuildContext context) => BasicoPage(),
           'scroll': (BuildContext context) => ScrollPage(),
+          'botones': (BuildContext context) => BotonesPage(),
         });
   }
 }
